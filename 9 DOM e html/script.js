0ad90380c4cb98ecfe3c67e0cb6ded2f6787fb0e
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2022-02-12 20:11:03 -0300

    Selecionando no DOM

--- 9 DOM e html/script.js
+++ 9 DOM e html/script.js
@@ -6,15 +6,54 @@ const animais = document.getElementById('animais')
 
 //podemos selecionar por classe e tagname tmb
 const gridSection = document.getElementsByClassName('grid-section')
-//por classe sempre retorna um array com os elementos
+//por classe sempre retorna um html collection 'array com os elementos'
 //podemos passar classes compostas
 
 //Existe um seletor geral o querySelector ele busca por css
 //se for classe . id # se for uma tag ul
 //ele seleciona o primeiro elemento que encontrar compativel
 const primeiraLi = document.querySelector('li')
+//o sinal ^significa  vai pegar o que comeca com
 const linkInterno = document.querySelector('[href^="#"]')
 
 //O querySelectorAll busca todos os elementos da pagina compativeis
 const animaisImg = document.querySelectorAll('.animais img')
-//isso não retorna um arrays e sim um NODE list
+//isso não retorna html colections e sim um NODE list que tmb é um tipo de array
+
+//diferença entre html collection e node list
+//primeira são os metodos e propriedades
+//html collection é ao vivo se apos a execução do codigo algo mudar como um
+//elemento ser criado e corresponder a busca isso retorna no html collection
+//ja no node list não
+
+//Exemplo for each
+//gridSection.forEach(function(gridItem, index, array) {
+
+// Retorne no console todas as imagens do site
+console.log('EXERCICIO 1')
+const todasImgs = document.querySelectorAll('img')
+todasImgs.forEach(function (item) {
+  console.log(item)
+})
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+console.log('EXERCICIO 2')
+const apenasImagem = document.querySelectorAll('img[src^="./img/imagem"]')
+apenasImagem.forEach(function (item) {
+  console.log(item)
+})
+
+// Selecione todos os links internos (onde o href começa com #)
+console.log('EXERCICIO 3')
+const apenasHref = document.querySelectorAll('[href^="#"]')
+apenasHref.forEach(function (item) {
+  console.log(item)
+})
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+const primeiroH2 = document.querySelector('.animais-descricao h2')
+
+// Selecione o último p do site
+
+const todosP = document.querySelectorAll('p')
+const ultimoP = todosP[todosP.length - 1]
