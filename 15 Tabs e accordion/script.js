e86b3dda47b6d2c5bc79521ab310a496c7a5be85
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2023-04-17 20:59:56 -0300

    17/04/2023

--- 15 Tabs e accordion/script.js
+++ 15 Tabs e accordion/script.js
@@ -22,13 +22,14 @@ function initTabNav() {
 initTabNav()
 function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt')
+  const activeClass = 'ativo'
   if (accordionList.length) {
-    accordionList[0].classList.add('ativo')
-    accordionList[0].nextElementSibling.classList.add('ativo')
+    accordionList[0].classList.add(activeClass)
+    accordionList[0].nextElementSibling.classList.add(activeClass)
 
     function activeAccordion() {
-      this.classList.toggle('ativo')
-      this.nextElementSibling.classList.toggle('ativo')
+      this.classList.toggle(activeClass)
+      this.nextElementSibling.classList.toggle(activeClass)
     }
 
     accordionList.forEach(item => {
